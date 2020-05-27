ca7cd614918e34ace03a96d6a98ecced77f4c77f
idgserpro <idg@serpro.gov.br>
2020-05-27 16:03:21 -0300

    Ajusta o version.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='brasil.gov.newfieldcomplement',
-    version='1.1.0.dev2.dev0',
+    version='1.1.0.dev2',
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
