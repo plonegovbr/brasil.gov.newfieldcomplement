a7980dbc00815a16629f9e7e6e6ed14d019ef7ba
idgserpro <idg@serpro.gov.br>
2020-05-28 12:13:04 -0300

    Back to development: 1.1.0.dev3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='brasil.gov.newfieldcomplement',
-    version='1.1.0.dev2',
+    version='1.1.0.dev3.dev0',
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
